5a8afef4788d0788cef0accf0e7480f27c102a43
gkdudquf <gkdudquf@gmail.com>
2023-03-14 13:37:57 +0900

    구구단 수정

--- src/ex/Example2.java
+++ src/ex/Example2.java
@@ -2,12 +2,12 @@ package ex;
 
 public class Example2 {
 	public void numberTable() {
-		for (int i = 2; i <= 9; i++) {
-			System.out.println(i + "단");
-			for (int j = 1; j <= 9; j++) {
-				System.out.println(i+" * "+j+" = "+(i*j));
-			}
-			System.out.println();
-		}
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(i + " x " + j + " = " + (i*j) + "\t");
+            }
+            System.out.println();
+        }
+		
 	}
 }
